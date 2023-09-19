72892dfc5c1a77fc950ea0360524d9efbd909ac9
rmusayevr <rmusayevr19@gmail.com>
2023-09-19 10:42:28 +0400

    register and login pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing, Error, Register, Dashboard } from './pages'
+import { Landing, Error, Register } from './pages';
+import { AllJobs, AddJob, Profile, SharedLayout, Stats } from './pages/Dashboard';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='landing' element={<Landing />} />
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
+import BigSidebar from './BigSidebar'
+import FormRow from './FormRow'
 import Logo from './Logo'
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
 
-export { Logo }
+export { FormRow, Logo, Navbar, BigSidebar, SmallSidebar }
