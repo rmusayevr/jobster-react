b30af6cf11514e96cb72f385dcf0ce9aba02cdba
rmusayevr <rmusayevr19@gmail.com>
2023-09-20 11:15:55 +0400

    add job, all jobs, test user, authentication pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import FormRow from './FormRow'
 import Logo from './Logo'
 import Navbar from './Navbar'
 import SmallSidebar from './SmallSidebar'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+import Job from './Job'
 
-export { FormRow, Logo, Navbar, BigSidebar, SmallSidebar }
+export { FormRow, Logo, Navbar, BigSidebar, SmallSidebar, SearchContainer, JobsContainer, Job }
--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -1,6 +1,83 @@
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import FormRowSelect from '../../components/FormRowSelect';
+import { clearValues, createJob, handleChange, editJob } from '../../features/job/jobSlice';
+import { useEffect } from 'react';
+
 const AddJob = () => {
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId
+  } = useSelector((state) => state.job);
+  const { user } = useSelector((state) => state.user)
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    };
+
+    if (isEditing) {
+      dispatch(editJob({
+        jobId: editJobId,
+        job: {
+          position,
+          company,
+          jobLocation,
+          jobType,
+          status
+        },
+      }));
+      return;
+    }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    dispatch(handleChange({ name, value }))
+  }
+
+  useEffect(() => {
+    if (!isEditing) {
+      dispatch(handleChange({ name: 'jobLocation', value: user.location }))
+    }
+  }, [])
+
   return (
-    <div>AddJob</div>
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        <div className='form-center'>
+          <FormRow type='text' name='position' value={position} handleChange={handleJobInput} />
+          <FormRow type='text' name='company' value={company} handleChange={handleJobInput} />
+          <FormRow type='text' labelText='job location' name='jobLocation' value={jobLocation} handleChange={handleJobInput} />
+          <FormRowSelect name='status' value={status} handleChange={handleJobInput} list={statusOptions} />
+          <FormRowSelect name='jobType' labelText='job type' value={jobType} handleChange={handleJobInput} list={jobTypeOptions} />
+          <div className='btn-container'>
+            <button type='button' className='btn btn-block clear-btn' onClick={() => dispatch(clearValues())}>
+              clear
+            </button>
+            <button type='submit' className='btn btn-block submit-btn' onClick={handleSubmit} disabled={isLoading}>
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 
